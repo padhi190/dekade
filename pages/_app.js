ead2bcaaa22b67b48fb370699e603aaaa915fa96
padhi190 <pitra.adhi@gmail.com>
2021-10-28 16:20:11 +0700

    fix navbar component reference error

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import NavBar from '../components/navbar';
+import NavBar from '../components/NavBar';
 import { UserContext } from '../lib/context';
 import { useUserData } from '../lib/hooks';
 
